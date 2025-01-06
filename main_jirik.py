3e0f39c67ec0f533ae981cc78bb0efc42a09daff
Martin-Jirik08 <mjirik@cmgp.cz>
2025-01-06 15:35:16 +0100

    Add files via upload

---
+++ main_jirik.py
@@ -0,0 +1,20 @@
+# Importing all app imports
+from config_jirik import *
+
+# Main Window Functions
+def calculate_cml(hmotnost, af):
+    bmr = hmotnost * 24.2
+    cml = bmr * af
+    cml = round(cml, 2)
+    return cml
+
+# Main Window Variables
+todays_date_str = dt.date.today().strftime("%d-%m-%Y") #this is a string
+todays_date_obj = dt.date.today() #this is an object
+
+# Main Window Lists / Dictionaries
+#import knihoven
+from random import randint
+
+
+
